fe4c77d065898e7a661faf5ca7dcda658f727a06
giang.phamnguyen <giang.phamnguyen@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-07-13 06:33:46 +0000

    remove redundant files and codes.

--- SouceCode/PlannerModule/src/openones/oopms/controller/PlannerController.java
+++ SouceCode/PlannerModule/src/openones/oopms/controller/PlannerController.java
@@ -1,15 +1,15 @@
 package openones.oopms.controller;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.portlet.ActionResponse;
 import javax.portlet.RenderRequest;
 
 import openones.oopms.dao.TaskDAO;
-import openones.oopms.form.HelloForm;
 import openones.oopms.form.PlannerForm;
-import openones.oopms.model.OopmsTask;
+import openones.oopms.model.Process;
+import openones.oopms.model.Tasks;
+import openones.oopms.model.Stage;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -24,9 +24,11 @@ import org.springframework.web.portlet.bind.annotation.RenderMapping;
 @Controller
 @RequestMapping("VIEW")
 public class PlannerController {
-    /** Logger for logging. */  
-    private static Logger log = Logger.getLogger(PlannerController.class);  
-    
+    /** Logger for logging. */
+    private static Logger log = Logger.getLogger(PlannerController.class);
+    private List<Tasks> taskList;
+    private List<Stage> stageList;
+    private List<Process> processList;
     /**
      * Create bean for form.
      * @return Form bean for UI.
@@ -34,61 +36,67 @@ public class PlannerController {
     @ModelAttribute("PlannerForm")
     public PlannerForm getCommandObject() {
         log.debug("getCommandObject.START");
-        
-        /*OopmsTask task1 = new OopmsTask();
-        task1.setTaskname("task number 01");
-        
-        OopmsTask task2 = new OopmsTask();
-        task1.setTaskname("task number 02");
-        
-        OopmsTask[] taskList = new OopmsTask [task1,task2};*/
-        
-               
+
         PlannerForm formBean = new PlannerForm();
         formBean.setTitle("sds");
-        //  formBean.setTaskList(taskList);
+        // formBean.setTaskList(taskList);
         return formBean;
     }
-    
-    
-    /** 
-     * Default screen. 
-     * @return name of view which is the name of the JSP page. 
-     */  
-/*    @RequestMapping  
-    public String initScreen() {  
-        log.debug("initScreen.START");
-        // Display TaskManager.jsp  
-        return "TaskManager";  
-    }*/ 
+
     @ActionMapping(params = "action=taskmanager")
     public void processPlanner(PlannerForm formBean, BindingResult result, SessionStatus status, ActionResponse response) {
         log.debug("processPlanner.START");
-        
+
     }
     @RenderMapping(params = "action=taskmanager")
     public ModelAndView postPlanner(PlannerForm formBean, RenderRequest request) {
         log.debug("postPlanner.START");
         // request.setAttribute("user2", formBean);
-        //set portlet session                    
-        
+        // set portlet session
+
         ModelAndView mav = new ModelAndView("TaskManager");
         TaskDAO taskDAO = new TaskDAO();
-        
-        
-        formBean.taskList = taskDAO.getAllTask();
-        
-       // log.debug(formBean.taskList[0].getTaskname());
-        mav.addObject("taskList", formBean.taskList);
-        
+
+        taskList = taskDAO.getAllTask();
+        stageList = taskDAO.getAllStage();
+        processList = taskDAO.getAllProcess();
+
+        // Convert StageID to string
+        for (int i = 0; i < taskList.size(); i++) {
+            for (int j = 0; j < stageList.size(); j++) {
+                if (taskList.get(i).getStageid().equals(stageList.get(j).getStageId())) {
+                    taskList.get(i).setStage_str(stageList.get(j).getName());
+                }
+            }
+        }
+        // Convert ProcessID to string
+        try {
+        for (int i = 0; i < taskList.size(); i++) {
+            for (int j = 0; j < processList.size(); j++) {
+                if (taskList.get(i).getProcessId().equals(processList.get(j).getProcessId())) {
+                    taskList.get(i).setProcess_str(processList.get(j).getName());
+                }
+            }
+        }
+        } catch (Exception ex) {
+            // TODO: handle exception
+            log.debug(processList.get(0).getName());
+            log.debug(taskList.get(0).getProcessId());
+            log.debug(processList.get(0).getProcessId());
+            log.error("Convert ProcessID to string", ex);
+        }
+        formBean.setTaskList(taskList);
+
+        mav.addObject("taskList", formBean.getTaskList());
+
         return mav;
     }
     @ActionMapping(params = "action=addTask")
     public void processAddTask(PlannerForm formBean, BindingResult result, SessionStatus status, ActionResponse response) {
         log.debug("processAddTask.START");
-        
-       log.debug("title=" + formBean.getTitle());
-  
+
+        log.debug("title=" + formBean.getTitle());
+
     }
 
 }
