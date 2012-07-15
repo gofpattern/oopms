f51236fecf43828ab895b41745b5b0e02d828cd7
giang.phamnguyen <giang.phamnguyen@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-07-15 19:58:38 +0000

    load "process","status", "stage","assigned to" to combo box

--- SouceCode/PlannerModule/src/openones/oopms/planner/dao/TaskDAO.java
+++ SouceCode/PlannerModule/src/openones/oopms/planner/dao/TaskDAO.java
@@ -2,9 +2,13 @@ package openones.oopms.planner.dao;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.math.BigDecimal;
 import java.util.List;
 
+import openones.oopms.planner.model.Assignment;
+import openones.oopms.planner.model.Developer;
 import openones.oopms.planner.model.Process;
+import openones.oopms.planner.model.ProjectStatus;
 import openones.oopms.planner.model.Stage;
 import openones.oopms.planner.model.Tasks;
 import openones.oopms.planner.utils.HibernateUtil;
@@ -24,17 +28,41 @@ public class TaskDAO {
     }
 
     @SuppressWarnings("unchecked")
+    public List<ProjectStatus> getAllStatus() {
+        log.debug("getAllStatus.START");
+        try {
+            session.getTransaction().begin();
+            String sql = "from ProjectStatus";
+            Query query = session.createQuery(sql);
+            List resultList = query.list();
+            List<ProjectStatus> statusList = (List<ProjectStatus>) resultList;
+            // session.flush();
+            // session.getTransaction().commit();
+            System.out.println("getAllStatus.end");
+            return statusList;
+            
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+
+            log.error("getAllStatus.ERROR", e);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
     public List<Tasks> getAllTask() {
         log.debug("getAllTask.START");
         try {
-            session.getTransaction().begin();
+            // session.getTransaction().begin();
             String sql = "from Tasks";
             Query query = session.createQuery(sql);
             List resultList = query.list();
             List<Tasks> taskList = (List<Tasks>) resultList;
             // session.flush();
             // session.getTransaction().commit();
-
+            System.out.println("getAllTask.end");
             return taskList;
         } catch (Exception e) {
             if (session.getTransaction().isActive()) {
@@ -66,7 +94,7 @@ public class TaskDAO {
 
             // session.flush();
             // session.getTransaction().commit();
-
+            System.out.println("getAllStage.end");
             return stageList;
         } catch (Exception e) {
             if (session.getTransaction().isActive()) {
@@ -88,10 +116,34 @@ public class TaskDAO {
 
             List<Process> processList = (List<Process>) resultList;
 
+            // session.flush();
+            // session.getTransaction().commit();
+            System.out.println("getAllProcess.end");
+            return processList;
+        } catch (Exception e) {
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            log.error("getAllProcess.Exception", e);
+        }
+        return null;
+    }
+
+    // Get developers belong to project
+    public List<Developer> getDeveloper(String projectId) {
+        log.debug("getAssignment.START");
+        try {
+            // session.getTransaction().begin();
+            String sql = "select developer from Assignment ass where ass.project.projectId = :projectId";
+            Query query = session.createQuery(sql);
+            query.setParameter("projectId", new BigDecimal(projectId));           
+            List<Developer> developerList =  query.list();
+
             session.flush();
             session.getTransaction().commit();
-
-            return processList;
+            
+            System.out.println("getAssignment.end");
+            return developerList;
         } catch (Exception e) {
             if (session.getTransaction().isActive()) {
                 session.getTransaction().rollback();
--- SouceCode/PlannerModule/src/openones/oopms/planner/form/PlannerForm.java
+++ SouceCode/PlannerModule/src/openones/oopms/planner/form/PlannerForm.java
@@ -20,6 +20,7 @@
 package openones.oopms.planner.form;
 
 import java.util.List;
+import java.util.Map;
 
 import openones.oopms.planner.model.Process;
 import openones.oopms.planner.model.Stage;
@@ -29,9 +30,116 @@ import openones.oopms.planner.model.Tasks;
  */
 public class PlannerForm {
     private String title;
+    private String projectId;
+    private String statusDefault;
+    private String stageDefault;
+    private String developerDefault;
+
     private List<Tasks> taskList;
     private List<Stage> stageList;
     private List<Process> processList;
+
+
+    Map<String, String> statusMap;
+    Map<String, String> stageMap;
+    Map<String, String> developerMap;
+    Map<String, String> processMap;
+
+    /**
+     * Get value of processMap.
+     * @return the processMap
+     */
+    public Map<String, String> getProcessMap() {
+        return processMap;
+    }
+
+    /**
+     * Set the value for processMap.
+     * @param processMap the processMap to set
+     */
+    public void setProcessMap(Map<String, String> processMap) {
+        this.processMap = processMap;
+    }
+
+    /**
+     * Get value of developerDefault.
+     * @return the developerDefault
+     */
+    public String getDeveloperDefault() {
+        return developerDefault;
+    }
+
+    /**
+     * Set the value for developerDefault.
+     * @param developerDefault the developerDefault to set
+     */
+    public void setDeveloperDefault(String developerDefault) {
+        this.developerDefault = developerDefault;
+    }
+
+    /**
+     * Get value of developerMap.
+     * @return the developerMap
+     */
+    public Map<String, String> getDeveloperMap() {
+        return developerMap;
+    }
+
+    /**
+     * Set the value for developerMap.
+     * @param developerMap the developerMap to set
+     */
+    public void setDeveloperMap(Map<String, String> developerMap) {
+        this.developerMap = developerMap;
+    }
+
+    /**
+     * Get value of projectId.
+     * @return the projectId
+     */
+    public String getProjectId() {
+        return projectId;
+    }
+
+    /**
+     * Set the value for projectId.
+     * @param projectId the projectId to set
+     */
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    /**
+     * Get value of stageDefault.
+     * @return the stageDefault
+     */
+    public String getStageDefault() {
+        return stageDefault;
+    }
+
+    /**
+     * Set the value for stageDefault.
+     * @param stageDefault the stageDefault to set
+     */
+    public void setStageDefault(String stageDefault) {
+        this.stageDefault = stageDefault;
+    }
+
+    /**
+     * Get value of stageMap.
+     * @return the stageMap
+     */
+    public Map<String, String> getStageMap() {
+        return stageMap;
+    }
+
+    /**
+     * Set the value for stageMap.
+     * @param stageMap the stageMap to set
+     */
+    public void setStageMap(Map<String, String> stageMap) {
+        this.stageMap = stageMap;
+    }
 
     public PlannerForm() {
 
@@ -43,6 +151,22 @@ public class PlannerForm {
      */
     public String getTitle() {
         return title;
+    }
+
+    /**
+     * Get value of statusDefault.
+     * @return the statusDefault
+     */
+    public String getStatusDefault() {
+        return statusDefault;
+    }
+
+    /**
+     * Set the value for statusDefault.
+     * @param statusDefault the statusDefault to set
+     */
+    public void setStatusDefault(String statusDefault) {
+        this.statusDefault = statusDefault;
     }
 
     /**
@@ -99,6 +223,22 @@ public class PlannerForm {
      */
     public void setProcessList(List<Process> processList) {
         this.processList = processList;
+    }
+
+    /**
+     * Get value of statusMap.
+     * @return the statusMap
+     */
+    public Map<String, String> getStatusMap() {
+        return statusMap;
+    }
+
+    /**
+     * Set the value for statusMap.
+     * @param statusMap the statusMap to set
+     */
+    public void setStatusMap(Map<String, String> statusMap) {
+        this.statusMap = statusMap;
     }
 
 }
