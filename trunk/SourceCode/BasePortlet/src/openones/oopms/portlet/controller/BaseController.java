804bb4fc9f12ec506ba7f6d6d6fc0927a62e7c20
thachln <thachln@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-07-22 16:46:08 +0000

    BasePortlet provides common functions of a portlet in OOPMS

---
+++ trunk/SourceCode/BasePortlet/src/openones/oopms/portlet/controller/BaseController.java
@@ -0,0 +1,123 @@
+/**
+ * Licensed to Open-Ones Group under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Open-Ones Group licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package openones.oopms.portlet.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletSession;
+import javax.portlet.RenderRequest;
+
+import openones.oopms.form.CommonInfo;
+import openones.oopms.form.SubMenu;
+import openones.oopms.form.UserInfo;
+import openones.oopms.util.BaseUtil;
+import openones.portlet.PortletSupport;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.portlet.ModelAndView;
+
+/**
+ * @author Thach.Le
+ */
+public class BaseController {
+    private static final String SK_USER_INFO = "UserInfo";
+    private static final String SK_COMMON_INFO = "CommonInfo";
+
+    /** Logger for logging. */
+    public final Logger log = Logger.getLogger(this.getClass());
+
+    // Get information of authenticated user: position
+    // UserInfo userInfo = new UserInfo();
+
+    public ModelAndView initScreen(RenderRequest request, PortletSession session) {
+        log.debug("initScreen.START");
+        PortletSupport portletSupport = new PortletSupport(request);
+        String logonUser = portletSupport.getLogonUser();
+
+        log.debug("logonUser=" + logonUser);
+
+        if (logonUser != null) {
+            // TODO: get Role, Group from the database
+            // Set roles for user
+            UserInfo userInfo = new UserInfo(logonUser);
+            userInfo.addRole("Developer");
+            userInfo.setGroup("Development");
+            userInfo.setLoginDate(BaseUtil.getCurrentDate());
+            // Update userInfo into the session
+            updateUserInfo(session, userInfo);
+        }
+
+        return null;
+    }
+    /**
+     * [Give the description for method].
+     * @param session
+     * @return
+     */
+    public UserInfo getUserInfo(PortletSession session) {
+        return (UserInfo) session.getAttribute(SK_USER_INFO);
+    }
+
+    /**
+     * [Give the description for method].
+     * @param session
+     * @param userInfo
+     */
+    public void updateUserInfo(PortletSession session, UserInfo userInfo) {
+        session.setAttribute(SK_USER_INFO, userInfo);
+    }
+
+    /**
+     * [Give the description for method].
+     * @param session
+     * @param commonInfo
+     */
+    public void updateCommonInfo(PortletSession session, CommonInfo commonInfo) {
+        session.setAttribute(SK_COMMON_INFO, commonInfo);
+    }
+
+    /**
+     * [Give the description for method].
+     * @param session
+     * @return
+     */
+    public CommonInfo getCommonInfo(PortletSession session) {
+        return (CommonInfo) session.getAttribute(SK_COMMON_INFO);
+    }
+
+    /**
+     * [Give the description for method].
+     * @param session
+     * @param projectMap
+     */
+    public void updateProjectMap(PortletSession session, Map<Integer, String> projectMap) {
+        CommonInfo commonInfo = getCommonInfo(session);
+
+        if (commonInfo == null) {
+            commonInfo = new CommonInfo();
+        }
+        commonInfo.setProjectMap(projectMap);
+        updateCommonInfo(session, commonInfo);
+    }
+
+    public void updateMenuBar(PortletSession session, List<SubMenu> subMenuList) {
+        session.setAttribute("MenuBar", subMenuList);
+    }
+}
