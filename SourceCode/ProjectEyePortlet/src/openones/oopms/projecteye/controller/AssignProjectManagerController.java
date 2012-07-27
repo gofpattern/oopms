7681a3bff5a1f419782bf8608488e02393e62178
tocongthanhhai <tocongthanhhai@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-07-27 05:01:52 +0000

    Fix bug

--- SourceCode/ProjectEyePortlet/src/openones/oopms/projecteye/controller/AssignProjectManagerController.java
+++ SourceCode/ProjectEyePortlet/src/openones/oopms/projecteye/controller/AssignProjectManagerController.java
@@ -59,8 +59,7 @@ public class AssignProjectManagerController {
 	/** Logger for logging. */
 	private static Logger log = Logger
 			.getLogger(AssignProjectManagerController.class);
-	
-	
+
 	@RenderMapping(params = "action=ChangeProjectManager")
 	public ModelAndView postChangeProjectManager(RenderRequest request) {
 		log.debug("post ChangeProjectManager.START");
@@ -68,51 +67,69 @@ public class AssignProjectManagerController {
 		String userId = request.getParameter("userId");
 		Assignment assignment = new Assignment();
 		AssignmentDao aDao = new AssignmentDao();
-        //set value for assgment
+
+		// set value for assgment
 		Project project = new Project();
 		project.setProjectId(new BigDecimal(projectId));
+		Assignment currentRole = aDao.getUserRole(project, new BigDecimal(
+				userId));
 		aDao.removeTeamMember(project, new BigDecimal(userId));
 		aDao.removeProjectManager(project);
 		assignment.setProject(project);
-        assignment.setDeveloperId(new BigDecimal(userId));
-        assignment.setType(new Byte(Constant.ProjectManagerType));
-        assignment.setBeginDate(new Date());
-        
-        aDao.insertAssigment(assignment);
-        DeveloperDao dDao = new DeveloperDao();
-        List<Developer> teamOfProject = dDao.getDeveloperTeamOfProject(project);
-        List<AssignProjectManagerForm> projectTeamList = new ArrayList<AssignProjectManagerForm>();
-        if(teamOfProject.size()>0) {
-        	for(int i=0; i<teamOfProject.size();i++) {
-        		AssignProjectManagerForm temp = new AssignProjectManagerForm();
-        		temp.setUserName(teamOfProject.get(i).getName());
-        		temp.setUserAccount(teamOfProject.get(i).getAccount());
-        		temp.setUserId(teamOfProject.get(i).getDeveloperId().toString());
-        		Assignment role = aDao.getUserRole(project, teamOfProject.get(i).getDeveloperId());
-        		temp.setUserRole(String.valueOf(role.getType()));
-        		if(Constant.ProjectOwnerAndProjectManagerType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("Project Owner and Project Manager");
-        		} else if (Constant.ProjectManagerType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("Project Manager");
-        		} else if (Constant.DeveloperType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("Developer");
-        		} else if (Constant.TesterType.equals(String.valueOf(role.getType()))){
-        			temp.setUserRoleString("Tester");
-        		} else if (Constant.QAType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("QA");
-        		} else if (Constant.CustomerType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("Customer");
-        		} else if (Constant.ProjectOwnerType.equals(String.valueOf(role.getType()))) {
-        			temp.setUserRoleString("Project Owner");
-        		}
-        		projectTeamList.add(temp);
-        	}
-        }
-        AssignProjectManagerForm form = new AssignProjectManagerForm();
-        ModelAndView mav = new ModelAndView("AssignProjectManager", "AssignProjectManagerForm", form);
-        mav.addObject("projectTeamList",projectTeamList);
-        log.debug("project ID la "+ projectId);
-        mav.addObject("projectId", projectId);
-        return mav;
+		assignment.setDeveloperId(new BigDecimal(userId));
+		if (Constant.ProjectOwnerType.equals(String.valueOf(currentRole
+				.getType()))) {
+			assignment.setType(new Byte(
+					Constant.ProjectOwnerAndProjectManagerType));
+		} else {
+			assignment.setType(new Byte(Constant.ProjectManagerType));
+		}
+		assignment.setBeginDate(new Date());
+
+		aDao.insertAssigment(assignment);
+		DeveloperDao dDao = new DeveloperDao();
+		List<Developer> teamOfProject = dDao.getDeveloperTeamOfProject(project);
+		List<AssignProjectManagerForm> projectTeamList = new ArrayList<AssignProjectManagerForm>();
+		if (teamOfProject.size() > 0) {
+			for (int i = 0; i < teamOfProject.size(); i++) {
+				AssignProjectManagerForm temp = new AssignProjectManagerForm();
+				temp.setUserName(teamOfProject.get(i).getName());
+				temp.setUserAccount(teamOfProject.get(i).getAccount());
+				temp.setUserId(teamOfProject.get(i).getDeveloperId().toString());
+				Assignment role = aDao.getUserRole(project, teamOfProject
+						.get(i).getDeveloperId());
+				temp.setUserRole(String.valueOf(role.getType()));
+				if (Constant.ProjectOwnerAndProjectManagerType.equals(String
+						.valueOf(role.getType()))) {
+					temp.setUserRoleString("Project Owner and Project Manager");
+				} else if (Constant.ProjectManagerType.equals(String
+						.valueOf(role.getType()))) {
+					temp.setUserRoleString("Project Manager");
+				} else if (Constant.DeveloperType.equals(String.valueOf(role
+						.getType()))) {
+					temp.setUserRoleString("Developer");
+				} else if (Constant.TesterType.equals(String.valueOf(role
+						.getType()))) {
+					temp.setUserRoleString("Tester");
+				} else if (Constant.QAType
+						.equals(String.valueOf(role.getType()))) {
+					temp.setUserRoleString("QA");
+				} else if (Constant.CustomerType.equals(String.valueOf(role
+						.getType()))) {
+					temp.setUserRoleString("Customer");
+				} else if (Constant.ProjectOwnerType.equals(String.valueOf(role
+						.getType()))) {
+					temp.setUserRoleString("Project Owner");
+				}
+				projectTeamList.add(temp);
+			}
+		}
+		AssignProjectManagerForm form = new AssignProjectManagerForm();
+		ModelAndView mav = new ModelAndView("AssignProjectManager",
+				"AssignProjectManagerForm", form);
+		mav.addObject("projectTeamList", projectTeamList);
+		log.debug("project ID la " + projectId);
+		mav.addObject("projectId", projectId);
+		return mav;
 	}
 }
