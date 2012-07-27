8a12136814e87d81726ec7591b6446f90730a4e6
giang.phamnguyen <giang.phamnguyen@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-07-27 17:34:51 +0000

    Del to change folder name

--- SourceCode/PlannerModule/deployPortlet.sh
+++
@@ -1,5 +0,0 @@
-export PORTAL_HOME=/home/thachle/jPackages/uPortal-4.0.5-quick-start/uPortal-4.0.5
-export WAR_FILEPATH=/home/thachle/Projects/Open-Ones/oopms/trunk/SourceCode/DMSPortlet/dist/DMSPortlet-0.0.1/DMSPortlet.war
-
-cd $PORTAL_HOME
-ant deployPortletApp -DportletApp="$WAR_FILEPATH"
