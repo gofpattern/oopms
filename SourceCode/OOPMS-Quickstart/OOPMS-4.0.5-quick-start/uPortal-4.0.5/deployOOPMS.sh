8045535b8d072892923fd03d075e05499e7c3cac
thachln <thachln@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-08-05 01:42:30 +0000

    Change port 8080 into 5050 Add script to compile and deploy the portal:  + Change context uPortal into oopms Remove deployed app uPortal

---
+++ SourceCode/OOPMS-Quickstart/OOPMS-4.0.5-quick-start/uPortal-4.0.5/deployOOPMS.sh
@@ -0,0 +1,3 @@
+ant deployPortletApp -DportletApp="./DeployPortlet/ProjectEyePortlet-0.0.1/ProjectEyePortlet.war"
+ant deployPortletApp -DportletApp="./DeployPortlet/DMSPortlet-0.0.1/DMSPortlet.war"
+ant deployPortletApp -DportletApp="./DeployPortlet/RequirementPortlet-0.0.1/RequirementPortlet.war"
