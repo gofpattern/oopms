c3aa6667f9242d181953518cea83d6f6d0dac277
thachln <thachln@c85c9dab-434b-e816-fbf5-fcd0f5e7b8b1>
2012-08-11 08:04:51 +0000

    Update script to compile, deploy Portlets: support new portlet DashboardPortlet

---
+++ trunk/SourceCode/clean.sh
@@ -0,0 +1,34 @@
+export PRJ=PlannerPortlet
+cd $PRJ
+ant clean
+cd ..
+
+
+export PRJ=TimesheetPortlet
+cd $PRJ
+ant clean
+cd ..
+
+
+export PRJ=DMSPortlet
+cd $PRJ
+ant clean
+cd ..
+
+
+export PRJ=ProjectEyePortlet
+cd $PRJ
+ant clean
+cd ..
+
+
+export PRJ=RequirementPortlet
+cd $PRJ
+ant clean
+cd ..
+
+
+export PRJ=DashboardPortlet
+cd $PRJ
+ant clean
+cd ..
